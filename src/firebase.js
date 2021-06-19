26528c1be8f7ff304d3b22376c0fb1c9e990dc62
victor_praise <vpraise27@gmail.com>
2021-06-19 19:15:31 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpM3Hrv5O1RZDAt_GryaSXxxcffXw63Qs",
+    authDomain: "whatsapp-clone-25dc6.firebaseapp.com",
+    projectId: "whatsapp-clone-25dc6",
+    storageBucket: "whatsapp-clone-25dc6.appspot.com",
+    messagingSenderId: "378584183339",
+    appId: "1:378584183339:web:0e0ae35754449176f6b06e",
+    measurementId: "G-SH9XP4573F"
+};
